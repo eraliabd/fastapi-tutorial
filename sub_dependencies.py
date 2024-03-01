bae627e12b28291676976b0e8036fff9c96d3707
eraliabd <eraliabdinazarov22@gmail.com>
2024-03-01 17:44:41 +0500

    feat: sub dependencies

--- sub_dependencies.py
+++ sub_dependencies.py
@@ -1,13 +1,7 @@
-from email._header_value_parser import get_value
-
 from fastapi import FastAPI, Depends, Body, Cookie
 from typing import Annotated
 
 app = FastAPI()
-
-
-async def needy_dependency(fresh_value: Annotated[str, Depends(get_value, use_cache=False)]):
-    return {"fresh_value": fresh_value}
 
 
 def query_extractor(q: str | None = None):
