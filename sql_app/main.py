dafd8d1702e209e0ffab8bd6a23ce056aeeeaae7
eraliabd <eraliabdinazarov22@gmail.com>
2024-03-13 07:07:53 +0500

    fix: sql (relational) database

--- sql_app/main.py
+++ sql_app/main.py
@@ -19,7 +19,7 @@ def get_db():
         db.close()
 
 
-@app.post("/users", response_model=schemas.User, status_code=201)
+@app.post("/users", response_model=schemas.User, status_code=201, tags=["users"])
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     print("db_user:", db_user)
@@ -28,13 +28,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users", response_model=list[schemas.User])
+@app.get("/users", response_model=list[schemas.User], tags=["users"])
 def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User, status_code=200)
+@app.get("/users/{user_id}", response_model=schemas.User, status_code=200, tags=["users"])
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -42,20 +42,20 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/items", response_model=schemas.Item, status_code=201)
+@app.post("/users/{user_id}/items", response_model=schemas.Item, status_code=201, tags=["items"])
 def create_item_for_user(
         user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_item(db, item=item, user_id=user_id)
 
 
-@app.get("/items", response_model=list[schemas.Item], status_code=200)
+@app.get("/items", response_model=list[schemas.Item], status_code=200, tags=["items"])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
 
-@app.get("/user/items", response_model=list[schemas.Item], status_code=200)
+@app.get("/users/{user_id}/items", response_model=list[schemas.Item], status_code=200, tags=["items"])
 def read_items_for_user(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items_for_user(db, user_id=user_id, skip=skip, limit=limit)
     return items
