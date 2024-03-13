763d01a74ccb0428fcb00aef1bf8ac7644df6edc
eraliabd <eraliabdinazarov22@gmail.com>
2024-03-13 06:55:30 +0500

    fix: sql (relational) database

--- sql_app/main.py
+++ sql_app/main.py
@@ -20,7 +20,7 @@ def get_db():
 
 
 @app.post("/users", response_model=schemas.User, status_code=201)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     print("db_user:", db_user)
     if db_user:
@@ -29,13 +29,13 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users", response_model=list[schemas.User])
-async def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User, status_code=200)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -43,19 +43,19 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/users/{user_id}/items", response_model=schemas.Item, status_code=201)
-async def create_item_for_user(
+def create_item_for_user(
         user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_item(db, item=item, user_id=user_id)
 
 
 @app.get("/items", response_model=list[schemas.Item], status_code=200)
-async def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
 
 @app.get("/user/items", response_model=list[schemas.Item], status_code=200)
-async def read_items_for_user(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items_for_user(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items_for_user(db, user_id=user_id, skip=skip, limit=limit)
     return items
