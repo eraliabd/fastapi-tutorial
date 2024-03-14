b40f35dd1b4149eb3aa7c2b6f08b81b32d35b4a9
eraliabd <eraliabdinazarov22@gmail.com>
2024-03-14 17:27:30 +0500

    background tasks

---
+++ background_tasks.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, BackgroundTasks, Depends
+import time
+
+app = FastAPI()
+
+
+# def write_notification(email: str, message=""):
+#     with open("log.txt", mode="w") as email_file:
+#         content = f"notification for {email}: {message}"
+#         time.sleep(5)
+#         email_file.write(content)
+#
+#
+# @app.post("/send_notification/{email}", status_code=202)
+# async def send_notification(email: str, background_tasks: BackgroundTasks):
+#     background_tasks.add_task(write_notification, email, message="some notification")
+#     return {"message": "Notification sent in the background"}
+
+
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+
+def get_query(background_tasks: BackgroundTasks, q: str | None = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
+
+
+@app.post("/send_notification/{email}")
+async def send_notification(
+        email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent", "query": q}
